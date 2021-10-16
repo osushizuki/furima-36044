0ad088e90bd9d35e52485577ccf6f5a1473b1162
osushizuki <imaokanorihiko@icloud.com>
2021-10-16 16:40:29 +0900

    ユーザー登録機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nick_name, :email, :birthday, :first_name, :last_name, :first_name_kanji, :last_name_kanji)
+  end
+end
