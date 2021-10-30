1e79b2b96cee80197018b3b1ea444018e60f21f9
osushizuki <imaokanorihiko@icloud.com>
2021-10-30 20:45:47 +0900

    ルーティング簡略化

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'items/index'
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :users, only: :show
+  resources :items
+
 end
