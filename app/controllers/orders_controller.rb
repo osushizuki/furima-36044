ecf8a971f35d05eef3015fe641007de56d6bc7d5
osushizuki <imaokanorihiko@icloud.com>
2021-11-04 21:03:45 +0900

    商品購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,26 @@
 class OrdersController < ApplicationController
+  
+  def index
+    @order_consumer = OrderConsumer.new
+    @item = Item.find(params[:item_id])
+  end
 
-        def index
-        end
+  def create
+    @order_consumer = OrderConsumer.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_consumer.valid?
+      @order_consumer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order_consumer).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 
 end
---
+++ app/models/order_consumer.rb
@@ -0,0 +1,17 @@
+class OrderConsumer
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
+
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, numericality: {only_integer: true}
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
+  def save
+    consumer = Consumer.create(user_id: user_id, item_id: item_id)
+    Order.create(postal_code: postal_code, prefecture_id: prefecture_id , city: city, address: address, building: building, phone_number: phone_number, consumer_id: consumer.id)
+  end
+end
